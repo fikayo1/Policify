323277fb92ebdb50cfecfbdc8c1b974e650ca898
Oluwafikayomi Soetan <soetandan@gmail.com>
2023-07-12 21:40:00 +0100

    added build scrpit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+cd source  # Move to the Django project directory
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
